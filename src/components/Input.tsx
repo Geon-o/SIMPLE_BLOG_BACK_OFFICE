b9e1960e8ee7d1727b0c36afc97b75f956eead7b
진건오 <shjwin96@mail.bizbee.ne.kr>
2025-08-22 17:27:21 +0900

    fix: 빌드 이슈 핸들링

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextField, TextFieldProps } from '@mui/material';
+import { TextField, type TextFieldProps } from '@mui/material';
 
 interface CustomInputProps {
     label: string;
