48c048aaad0d488cfbf0c6d1dd77adee1ac486be
진건오 <shjwin96@mail.bizbee.ne.kr>
2025-08-22 15:55:07 +0900

    fix: 이슈 핸들링

--- src/apis/SupabaseClient.ts
+++ src/apis/SupabaseClient.ts
@@ -3,8 +3,12 @@ import {createClient} from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-    throw new Error('에러발생');
+if (!supabaseAnonKey) {
+    throw new Error('anon 키 없음');
+}
+
+if (!supabaseUrl) {
+    throw new Error('url 없음');
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
