9d5e169a4295a253eecdac54c453e261c17ff62e
진건오 <shjwin96@mail.bizbee.ne.kr>
2025-08-22 17:19:27 +0900

    feat: 하위 카테고리페이지 생성 및 누르면 이동할 수 잇도록 설정

---
+++ src/pages/CategoryManagementPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const CategoryManagementPage = () => {
+  return (
+    <Typography variant="h4">카테고리 관리 페이지</Typography>
+  );
+};
+
+export default CategoryManagementPage;
