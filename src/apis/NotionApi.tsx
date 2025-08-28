2a38c976d42385234d30a99ea8a6ce2fd68c30de
진건오 <shjwin96@mail.bizbee.ne.kr>
2025-08-28 10:00:51 +0900

    feat: 카테고리 추가/수정/삭제 기능 추가

--- src/apis/NotionApi.tsx
+++ src/apis/NotionApi.tsx
@@ -12,8 +12,39 @@ export default function NotionApi() {
         return res.data.results;
     };
 
+    const saveCategoryApi = async (saveData) => {
+        const res = await axios.post('https://notion-proxy-api.vercel.app/api/saveCategory', {saveData: saveData}, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        return res.data;
+    };
+
+    const editCategoryApi = async (editData, pageId) => {
+        const res = await axios.post('https://notion-proxy-api.vercel.app/api/editCategory', {editData: editData, pageId: pageId}, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        return res.data;
+    }
+
+    const deleteCategoryApi = async (pageId) => {
+        const res = await axios.post('https://notion-proxy-api.vercel.app/api/deleteCategory', {pageId: pageId}, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        return res.data;
+    }
+
     return {
         categoryListApi,
-        subCategoryListApi
+        subCategoryListApi,
+        saveCategoryApi,
+        editCategoryApi,
+        deleteCategoryApi
     }
 }
